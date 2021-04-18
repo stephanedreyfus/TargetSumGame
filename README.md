88d43885f87df08586408978404c861bb51d7005
Stéphane Dreyfus <floatingrock@gmail.com>
2021-04-17 20:48:44 -0700

    Basic functionality of game complete.

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Written using VSCode while running an Android simulator.
 
 TODOS:
 - Get those colors right!
+- CSS work.
+- When you play again, make the game harder. Change the parameters sent to the game.
+- Keep a score. If win, score. Persists between games. Where to place score. How to control?
 
 [1]:(https://www.linkedin.com/learning/react-native-essential-training/)
 [2]:(https://github.com/samerbuna)
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import RandomNumber from './RandomNumber';
 import shuffle from 'lodash.shuffle';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
 class Game extends React.Component {
   static propTypes = {
     randomNumberCount: PropTypes.number.isRequired,
     initialSeconds: PropTypes.number.isRequired,
+    onPlayAgain: PropTypes.func.isRequired,
   };
   state = {
     selectedIds: [],
@@ -99,6 +100,9 @@ class Game extends React.Component {
             />
           )}
         </View>
+        {this.gameStatus !== 'PLAYING' && (
+          <Button title='Play Again' onPress={this.props.onPlayAgain} />
+        )}
         <Text style={[styles.counter, styles[`STATUS_${gameStatus}`]]}>
           {`You Have ${this.state.remainingSeconds} seconds remaining!`}
         </Text>
